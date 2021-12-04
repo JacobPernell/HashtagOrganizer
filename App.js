8ebe89c5855ba8b25387706cbb524e46f79f1794
Jacob Pernell <jacob.pernell@gmail.com>
2021-12-04 15:39:43 -0800

    Set up base elements of app

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [textArea, setTextArea] = useState('');
+    const [testCopy, setTestCopy] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <Text>Enter hashtags you want to save and reuse, and press the button to copy.</Text>
+
+            <TextInput style={styles.textInput} value={textArea} onChangeText={setTextArea}></TextInput>
+
+            <Pressable
+                onPress={() => {
+                    setTestCopy('Copied!');
+                }}
+                style={styles.button}
+            >
+                <Text>Yuhhhh</Text>
+            </Pressable>
+            <Text>{testCopy}</Text>
+
+            <Text>Testing text updating: {textArea}</Text>
+
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        marginLeft: 20,
+        justifyContent: 'center',
+    },
+    button: {
+        padding: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    textInput: {
+        marginTop: 10,
+        marginBottom: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+        width: '100%',
+    },
 });
